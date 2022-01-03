7fc0a419da44cc2e116f97c21169343953e303d9
Wouter Heyvaert <23416063+wohe157@users.noreply.github.com>
2022-01-03 13:00:12 +0100

    Using pytest instead of unittest

--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -1,179 +1,142 @@
-import unittest
+import pytest
 import heliq
 
 import numpy as np
 
 
-class CenterOfMassTestCase(unittest.TestCase):
-
-    def setUp(self):
-        # Create a cylinder at a known location
-        x, y, z = np.meshgrid(np.arange(64), np.arange(64), np.arange(64))
-        self.center = np.array((30, 35, 29))
-        self.data = np.logical_and(
-            (x - self.center[0])**2 + (y - self.center[1])**2 <= 10**2,
-            np.abs(z - self.center[2]) <= 20).astype(float)
-
-    def test_HandlesValidInputArrays(self):
-        """Test that centerOfMass doesn't raise exceptions when provided with correct data"""
-        try:
-            heliq.centerOfMass(self.data)
-        except:
-            self.fail("Exception raised when provided with 3D numpy array")
-
-    def test_HandlesInvalidInputArrays(self):
-        """Test that centerOfMass does raise exceptions when provided with incorrect data"""
-        with self.assertRaises(ValueError, msg="Doesn't complain about 1D data"):
-            heliq.centerOfMass(np.zeros((3,)))
-        with self.assertRaises(ValueError, msg="Doesn't complain about 2D data"):
-            heliq.centerOfMass(np.zeros((3,6)))
-        with self.assertRaises(ValueError, msg="Doesn't complain about 4D data"):
-            heliq.centerOfMass(np.zeros((3,2,9,1)))
-
-    def test_ReturnsArray(self):
-        """Test that centerOfMass returns a numpy array with shape (3,)"""
-        center = heliq.centerOfMass(self.data)
-        self.assertEqual(np.ndarray, type(center), "Output is not a numpy array")
-        self.assertEqual(1, center.ndim, "Output is not a 1D array")
-        self.assertEqual(3, center.shape[0], "Output is not a vector with 3 elements")
-
-    def test_CalculatesCorrectCenter(self):
-        """Test that the calculated center of mass is correct"""
-        center = heliq.centerOfMass(self.data)
-        for i in range(3):
-            self.assertAlmostEqual(self.center[i], center[i], "Center is not calculated correctly")
+@pytest.fixture
+def offsetCylinder():
+    """Create a cylinder at a known location"""
+    x, y, z = np.meshgrid(np.arange(64), np.arange(64), np.arange(64))
+    center = np.array((30, 35, 29))
+    data = np.logical_and(
+        (x - center[0])**2 + (y - center[1])**2 <= 10**2,
+        np.abs(z - center[2]) <= 20).astype(float)
+    return center, data
 
 
-class HelicalAxisPcaTestCase(unittest.TestCase):
-
-    def setUp(self):
-        # Create an aligned cylinder at the center
-        ls = np.linspace(-1, 1, 64)
-        x, y, z = np.meshgrid(ls, ls, ls)
-        self.data = np.logical_and(x**2 + y**2 <= 0.3**2, np.abs(z) <= 0.6).astype(float)
-
-    def test_HandlesValidInputArrays(self):
-        """Test that helicalAxisPca doesn't raise exceptions when provided with correct data"""
-        try:
-            heliq.helicalAxisPca(self.data, 0.5)
-        except:
-            self.fail("Exception raised when provided with 3D numpy array")
-
-    def test_HandlesInvalidInputArrays(self):
-        """Test that helicalAxisPca does raise exceptions when provided with incorrect data"""
-        with self.assertRaises(ValueError, msg="Doesn't complain about 1D data"):
-            heliq.helicalAxisPca(np.zeros((3,)), 0.5)
-        with self.assertRaises(ValueError, msg="Doesn't complain about 2D data"):
-            heliq.helicalAxisPca(np.zeros((3,6)), 0.5)
-        with self.assertRaises(ValueError, msg="Doesn't complain about 4D data"):
-            heliq.helicalAxisPca(np.zeros((3,2,9,1)), 0.5)
-
-    def test_ReturnsArray(self):
-        """Test that helicalAxisPca returns a numpy array with shape (3,)"""
-        orientation = heliq.helicalAxisPca(self.data, 0.5)
-        self.assertEqual(np.ndarray, type(orientation), "Output is not a numpy array")
-        self.assertEqual(1, orientation.ndim, "Output is not a 1D array")
-        self.assertEqual(3, orientation.shape[0], "Output is not a vector with 3 elements")
-
-    def test_HandlesPrealignedData(self):
-        """Test that the calculated orientation is correct when the object is already aligned with z"""
-        orientation = heliq.helicalAxisPca(self.data, 0.5)
-        expected = np.array([0, 0, 1])
-        for i in range(3):
-            self.assertAlmostEqual(expected[i], orientation[i],
-                                   "Orientation of prealigned data is not calculated correctly")
-
-    def test_HandlesMisalignedData(self):
-        """Test that the calculated orientation is correct when the object is not aligned with z"""
-        orientation = heliq.helicalAxisPca(self.data.transpose((2, 1, 0)), 0.5)
-        expected = np.array([0, 1, 0])
-        for i in range(3):
-            self.assertAlmostEqual(expected[i], orientation[i],
-                                   "Orientation of data aligned to y-axis is not calculated correctly")
-
-        orientation = heliq.helicalAxisPca(self.data.transpose((0, 2, 1)), 0.5)
-        expected = np.array([1, 0, 0])
-        for i in range(3):
-            self.assertAlmostEqual(expected[i], orientation[i],
-                                   "Orientation of data aligned to x-axis is not calculated correctly")
+@pytest.fixture
+def alignedCylinder():
+    """Create an aligned cylinder at the center"""
+    ls = np.linspace(-1, 1, 64)
+    x, y, z = np.meshgrid(ls, ls, ls)
+    return np.logical_and(x**2 + y**2 <= 0.3**2, np.abs(z) <= 0.6).astype(float)
 
 
-class AlignHelicalAxisTestCase(unittest.TestCase):
+def test_centerOfMass_handlesValidInputArrays(offsetCylinder):
+    heliq.centerOfMass(offsetCylinder[1])
 
-    def setUp(self):
-        # Create an aligned cylinder at the center
-        ls = np.linspace(-1, 1, 64)
-        x, y, z = np.meshgrid(ls, ls, ls)
-        self.data = np.logical_and(x**2 + y**2 <= 0.3**2, np.abs(z) <= 0.6).astype(float)
 
-    def test_HandlesValidDataOrientationAndCenter(self):
-        """Test that alignHelicalAxis doesn't raise exceptions when provided with input arguments"""
-        try:
-            heliq.alignHelicalAxis(self.data, np.array((0, 0, 1)), np.array((0, 0, 0)))
-        except:
-            self.fail("Exception raised when provided with 3D numpy array")
+@pytest.mark.parametrize("shape", [
+    pytest.param((3,), id="1D"),
+    pytest.param((3,6), id="2D"),
+    pytest.param((2,2,9,1), id="4D"),
+])
+def test_centerOfMass_handlesInvalidInputArrays(shape):
+    with pytest.raises(ValueError):
+        heliq.centerOfMass(np.zeros(shape))
 
-    def test_HandlesTupleOrListAsOrientation(self):
-        """Test that alignHelicalAxis correctly handles orientation if it is provided as a tuple or list"""
-        try:
-            heliq.alignHelicalAxis(self.data, (0, 0, 1), np.array((0, 0, 0)))
-        except:
-            self.fail("Exception raised when provided with tuple")
 
-        try:
-            heliq.alignHelicalAxis(self.data, [0, 0, 1], np.array((0, 0, 0)))
-        except:
-            self.fail("Exception raised when provided with list")
+def test_centerOfMass_returnsArray(offsetCylinder):
+    center = heliq.centerOfMass(offsetCylinder[1])
+    assert type(center) is np.ndarray
+    assert center.ndim == 1
+    assert center.shape[0] == 3
 
-    def test_HandlesTupleOrListAsCenter(self):
-        """Test that alignHelicalAxis correctly handles center if it is provided as a tuple or list"""
-        try:
-            heliq.alignHelicalAxis(self.data, np.array((0, 0, 1)), (0, 0, 0))
-        except:
-            self.fail("Exception raised when provided with tuple")
 
-        try:
-            heliq.alignHelicalAxis(self.data, np.array((0, 0, 1)), [0, 0, 0])
-        except:
-            self.fail("Exception raised when provided with list")
+def test_centerOfMass_calculatesCorrectCenter(offsetCylinder):
+    center = heliq.centerOfMass(offsetCylinder[1])
+    for i in range(3):
+        assert center[i] == offsetCylinder[0][i]
 
-    def test_HandlesInvalidData(self):
-        """Test that alignHelicalAxis does raise exceptions when provided with incorrect data"""
-        with self.assertRaises(ValueError, msg="Doesn't complain about 1D data"):
-            heliq.alignHelicalAxis(np.zeros((3,)), (0, 0, 1), (0, 0, 0))
-        with self.assertRaises(ValueError, msg="Doesn't complain about 2D data"):
-            heliq.alignHelicalAxis(np.zeros((3,6)), (0, 0, 1), (0, 0, 0))
-        with self.assertRaises(ValueError, msg="Doesn't complain about 4D data"):
-            heliq.alignHelicalAxis(np.zeros((3,2,9,1)), (0, 0, 1), (0, 0, 0))
 
-    def test_HandlesInvalidOrientation(self):
-        """Test that alignHelicalAxis does raise exceptions when provided with incorrect orientation"""
-        with self.assertRaises(ValueError, msg="Doesn't complain about 1D orientation"):
-            heliq.alignHelicalAxis(self.data, (1,), (0, 0, 0))
-        with self.assertRaises(ValueError, msg="Doesn't complain about 2D orientation"):
-            heliq.alignHelicalAxis(self.data, (0, 1), (0, 0, 0))
-        with self.assertRaises(ValueError, msg="Doesn't complain about 4D orientation"):
-            heliq.alignHelicalAxis(self.data, (0, 0, 1, 0), (0, 0, 0))
+def test_helicalAxisPca_handlesValidInputArrays(alignedCylinder):
+    heliq.helicalAxisPca(alignedCylinder, 0.5)
 
-    def test_HandlesInvalidCenter(self):
-        """Test that alignHelicalAxis does raise exceptions when provided with incorrect center"""
-        with self.assertRaises(ValueError, msg="Doesn't complain about 1D orientation"):
-            heliq.alignHelicalAxis(self.data, (0, 0, 1), (0,))
-        with self.assertRaises(ValueError, msg="Doesn't complain about 2D orientation"):
-            heliq.alignHelicalAxis(self.data, (0, 0, 1), (0, 0))
-        with self.assertRaises(ValueError, msg="Doesn't complain about 4D orientation"):
-            heliq.alignHelicalAxis(self.data, (0, 0, 1), (0, 0, 0, 0))
 
-    def test_RequiresNonzeroOrientation(self):
-        """Test that alignHelicalAxis complains when an orientation with all zeros is given"""
-        with self.assertRaises(ValueError, msg="Doesn't complain about zero orientation"):
-            heliq.alignHelicalAxis(self.data, (0, 0, 0), (0, 0, 0))
+@pytest.mark.parametrize("shape", [
+    pytest.param((3,), id="1D"),
+    pytest.param((3, 6), id="2D"),
+    pytest.param((2, 2, 9, 1), id="4D"),
+])
+def test_helicalAxisPca_handlesInvalidInputArrays(shape):
+    with pytest.raises(ValueError):
+        heliq.helicalAxisPca(np.zeros(shape), 0.5)
 
-    def test_ReturnsSameShapeAsInput(self):
-        """Test that alignHelicalAxis returns an array with the same shape and type as the input array"""
-        # Don't care about the correct center or orientation in this test
-        aligned = heliq.alignHelicalAxis(self.data, (0, 0, 1), (0, 0, 0))
-        self.assertEqual(self.data.dtype, aligned.dtype, "Output type is not same as input type")
-        for i in range(3):
-            self.assertEqual(self.data.shape[i], aligned.shape[i],
-                             "Output shape is not same as input shape")
+
+def test_helicalAxisPca_returnsArray(alignedCylinder):
+    orientation = heliq.helicalAxisPca(alignedCylinder, 0.5)
+    assert type(orientation) is np.ndarray
+    assert orientation.ndim == 1
+    assert orientation.shape[0] == 3
+
+
+def test_helicalAxisPca_handlesPrealignedData(alignedCylinder):
+    orientation = heliq.helicalAxisPca(alignedCylinder, 0.5)
+    expected = np.array([0, 0, 1])
+    for i in range(3):
+        assert orientation[i] == pytest.approx(expected[i])
+
+
+def test_helicalAxisPca_handlesMisalignedData(alignedCylinder):
+    orientation = heliq.helicalAxisPca(alignedCylinder.transpose((2, 1, 0)), 0.5)
+    expected = np.array([0, 1, 0])
+    for i in range(3):
+        assert orientation[i] == pytest.approx(expected[i])
+
+    orientation = heliq.helicalAxisPca(alignedCylinder.transpose((0, 2, 1)), 0.5)
+    expected = np.array([1, 0, 0])
+    for i in range(3):
+        assert orientation[i] == pytest.approx(expected[i])
+
+
+@pytest.mark.parametrize("orientation,center", [
+    pytest.param(np.array((0, 0, 1)), np.array((0, 0, 0)), id="arrays"),
+    pytest.param((0, 0, 1), (0, 0, 0), id="tuples"),
+    pytest.param([0, 0, 1], [0, 0, 0], id="lists"),
+])
+def test_alignHelicalAxis_handlesValidArgs(alignedCylinder, orientation, center):
+    heliq.alignHelicalAxis(alignedCylinder, orientation, center)
+
+
+@pytest.mark.parametrize("shape", [
+    pytest.param((3,), id="1D"),
+    pytest.param((3, 6), id="2D"),
+    pytest.param((2, 2, 9, 1), id="4D"),
+])
+def test_alignHelicalAxis_handlesInvalidData(shape):
+    with pytest.raises(ValueError):
+        heliq.alignHelicalAxis(np.zeros(shape), (0, 0, 1), (0, 0, 0))
+
+
+@pytest.mark.parametrize("orientation", [
+    pytest.param((1,), id="1D"),
+    pytest.param((0, 1), id="2D"),
+    pytest.param((0, 0, 1, 0), id="4D"),
+])
+def test_alignHelicalAxis_handlesInvalidOrientation(alignedCylinder, orientation):
+    with pytest.raises(ValueError):
+        heliq.alignHelicalAxis(alignedCylinder, orientation, (0, 0, 0))
+
+
+@pytest.mark.parametrize("center", [
+    pytest.param((0,), id="1D"),
+    pytest.param((0, 0), id="2D"),
+    pytest.param((0, 0, 0, 0), id="4D"),
+])
+def test_alignHelicalAxis_handlesInvalidCenter(alignedCylinder, center):
+    with pytest.raises(ValueError):
+        heliq.alignHelicalAxis(alignedCylinder, (0, 0, 1), center)
+
+
+def test_alignHelicalAxis_requiresNonzeroOrientation(alignedCylinder):
+    with pytest.raises(ValueError):
+        heliq.alignHelicalAxis(alignedCylinder, (0, 0, 0), (0, 0, 0))
+
+
+def test_alignHelicalAxis_returnsSameShapeAsInput(alignedCylinder):
+    # Don't care about the correct center or orientation in this test
+    aligned = heliq.alignHelicalAxis(alignedCylinder, (0, 0, 1), (0, 0, 0))
+    assert aligned.dtype is alignedCylinder.dtype
+    for i in range(3):
+        assert aligned.shape[i] == alignedCylinder.shape[i]
