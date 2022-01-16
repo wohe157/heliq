ef25506fed2eeb7e2d168bee86d703cb33ed896b
Wouter Heyvaert <23416063+wohe157@users.noreply.github.com>
2022-01-16 10:23:01 +0100

    Improved implementation of helicity.py

--- src/heliq/helicity.py
+++ src/heliq/helicity.py
@@ -25,13 +25,42 @@ from matplotlib.image import AxesImage
 from typing import Tuple
 
 
-# TODO Cython doesn't work properly with dataclasses yet
 @dataclasses.dataclass
 class HelicityFunction:
     """Holds the contents of a helicity function."""
     delta_alpha: float
     delta_rho: float
     histogram: np.ndarray
+
+
+def _cylindrical_coordinates(shape):
+    x, y, z = np.meshgrid(np.arange(shape[1]) - shape[1] / 2,
+                          np.arange(shape[0]) - shape[0] / 2,
+                          np.arange(shape[2]) - shape[2] / 2)
+    rho = np.sqrt(x ** 2 + y ** 2)
+    phi = np.arctan2(y, x)
+    return rho, phi, z
+
+
+def _gradient_3d(data, kernel_size):
+    assert data.ndim == 3
+    assert np.max(data) > np.min(data)
+    assert kernel_size > 1 and kernel_size % 2 == 1
+
+    # The gradient is calculated using Sobel filters
+    s = np.arange(kernel_size, dtype=float) - kernel_size // 2
+    sx, sy, sz = np.meshgrid(s, s, s)
+    sr = sx ** 2 + sy ** 2 + sz ** 2
+    # prevent division by zero, of which the result should be zero in this case
+    sr[sr == 0] = np.Inf
+
+    # For some reason, this custom implementation that doesn't use the
+    # separability of the Sobel kernel is about 2x faster than the Scipy
+    # implementation
+    gx = scipy.ndimage.convolve(data, sx / sr)
+    gy = scipy.ndimage.convolve(data, sy / sr)
+    gz = scipy.ndimage.convolve(data, sz / sr)
+    return gx, gy, gz
 
 
 def helicity_descriptor(data: np.ndarray,
@@ -55,41 +84,25 @@ def helicity_descriptor(data: np.ndarray,
         in each voxel and the second element is the inclination angle in
         degrees in each voxel.
     """
-    assert data.ndim == 3
-    assert np.max(data) > np.min(data)
-    assert kernel_size > 1 and kernel_size % 2 == 1
-
-    x, y, z = np.meshgrid(np.arange(data.shape[1]) - data.shape[1] / 2,
-                          np.arange(data.shape[0]) - data.shape[0] / 2,
-                          np.arange(data.shape[2]) - data.shape[2] / 2)
-    phi = np.arctan2(y, x)
-
-    # The gradient is calculated along cylindrical planes using Sobel filters
-    s = np.arange(kernel_size, dtype=float) - kernel_size // 2
-    sx, sy, sz = np.meshgrid(s, s, s)
-    sr = sx ** 2 + sy ** 2 + sz ** 2
-    # prevent division by zero, of which the result should be zero in this case
-    sr[sr == 0] = np.Inf
-
-    # For some reason, this custom implementation that doesn't use the
-    # separability of the Sobel kernel is about 2x faster than the Scipy
-    # implementation
-    gx = scipy.ndimage.convolve(data, sx / sr)
-    gy = scipy.ndimage.convolve(data, sy / sr)
-    gz = scipy.ndimage.convolve(data, sz / sr)
+    # Calculate the gradient and project it on cylindrical planes
+    gx, gy, gz = _gradient_3d(data, kernel_size)
+    _, phi, _ = _cylindrical_coordinates(data.shape)
     gphi = - gx * np.sin(phi) + gy * np.cos(phi)
 
-    # gmag should be normalized such that the helicity function is bounded to
-    # [-1, +1]
-    alpha = np.arctan2(gphi, gz) * 180 / np.pi
+    # alpha should be in the range [-90°, 90°] because inclination angles with
+    # a difference of n*180° are equivalent, and gmag should be normalized such
+    # that the helicity function is bounded to [-1, +1]
+    alpha = 90 + np.arctan2(gz, gphi) * 180 / np.pi
+    alpha[alpha > 90] -= 180
     gmag = np.sqrt(gphi ** 2 + gz ** 2)
     gmag /= np.sum(np.abs(gmag))
     return gmag, alpha
 
 
 def helicity_function(data: np.ndarray,
+                      voxel_size: float,
                       delta_alpha: float = 1.0,
-                      delta_rho: float = 1.0,
+                      delta_rho: float = None,
                       kernel_size: int = 5,
                       ) -> HelicityFunction:
     """Calculate the helicity function for a given object.
@@ -97,10 +110,13 @@ def helicity_function(data: np.ndarray,
     Args:
         data:
             The voxel data as a 3D array that describes the object.
+        voxel_size:
+            The width of the voxels in the data.
         delta_alpha:
             The bin size for the inclination angles in degrees.
         delta_rho:
-            The bin size in the radial direction.
+            The bin size in the radial direction in the same units as the
+            voxel_size, by default ``delta_rho = voxel_size``.
         kernel_size:
             The size of the Sobel kernel used for calculating the gradient,
             should be an odd integer.
@@ -108,41 +124,42 @@ def helicity_function(data: np.ndarray,
     Returns:
         A ``HelicityFunction`` containing the results.
     """
+    if delta_rho is None:
+        delta_rho = voxel_size
+
+    assert voxel_size > 0
     assert delta_alpha > 0
     assert delta_rho > 0
 
     # For binning we need the inclination angle and gradient magnitude, but the
     # gradient magnitude should have the correct sign (+ for right-handed and -
-    # for left-handed). The orientation of a feature is orthogonal to the
-    # orientation of its gradient, the inclination angle is therefore
-    # 90° + atan(gz/gphi) = atan(gphi/gz)
+    # for left-handed). After setting the correct sign, we are only interested
+    # in the absolute value of the inclination angle.
     gmag, alpha = helicity_descriptor(data, kernel_size)
     gmag *= np.sign(alpha)
     alpha = np.abs(alpha)
 
-    x, y, z = np.meshgrid(np.arange(data.shape[1]) - data.shape[1] / 2,
-                          np.arange(data.shape[0]) - data.shape[0] / 2,
-                          np.arange(data.shape[2]) - data.shape[2] / 2)
-    rho = np.sqrt(x ** 2 + y ** 2)
+    rho, _, _ = _cylindrical_coordinates(data.shape)
+    rho *= voxel_size
 
-    max_rho = float(min(data.shape[:2]))
-    rho_edges_left = np.arange(0., max_rho, delta_rho)  # right = left + delta
-    alpha_edges_left = np.arange(0., 90., delta_alpha)
-    bins = np.zeros((len(rho_edges_left), len(alpha_edges_left)), dtype=float)
+    nbins_rho = int(np.ceil(np.max(rho) / delta_rho))
+    nbins_alpha = int(np.ceil(90. / delta_alpha))
+    bins = np.zeros((nbins_rho, nbins_alpha), dtype=float)
 
-    for i, rho_edge_left in enumerate(rho_edges_left):
-        for j, alpha_edge_left in enumerate(alpha_edges_left):
-            bins[i, j] = np.sum(gmag[np.logical_and(
-                np.logical_and(alpha > alpha_edge_left,
-                               alpha <= alpha_edge_left + delta_alpha),
-                np.logical_and(rho > rho_edge_left,
-                               rho <= rho_edge_left + delta_rho))])
+    m = np.floor_divide(rho, delta_rho).astype(int)
+    n = np.floor_divide(alpha, delta_alpha).astype(int)
+    for i in range(gmag.shape[0]):
+        for j in range(gmag.shape[1]):
+            for k in range(gmag.shape[2]):
+                mm = m[i, j, k]
+                nn = n[i, j, k]
+                bins[mm, nn] += gmag[i, j, k]
 
     # The helicity function should be divided by the bin area, which makes it
     # possible to compare intensities between helicity functions with different
     # bin sizes. This does not influence the total helicity, because it takes
     # this into account already in the integration.
-    bins /= (delta_rho * delta_alpha)
+    bins /= (voxel_size * delta_rho * delta_alpha)
     return HelicityFunction(delta_alpha, delta_rho, bins)
 
 
@@ -180,7 +197,7 @@ def plot_helicity_function(hfunc: HelicityFunction,
     im = axis.imshow(hfunc.histogram,
                      interpolation='nearest',
                      origin='lower',
-                     extent=(0, 90, 0, hfunc.histogram.shape[0]),
+                     extent=(0, 90, 0, hfunc.histogram.shape[0] * hfunc.delta_rho),
                      cmap=cmap)
     im.set_clim(-vmax, vmax)
     axis.set_aspect('auto')
--- tests/helicity/test_helicity_function.py
+++ tests/helicity/test_helicity_function.py
@@ -4,15 +4,15 @@ import numpy as np
 
 
 def test_accepts_positive_delta_rho():
-    heliq.helicity_function(np.random.standard_normal((2, 2, 2)), delta_rho=3.1)
+    heliq.helicity_function(np.random.standard_normal((2, 2, 2)), 1., delta_rho=3.1)
 
 
 def test_accepts_positive_delta_alpha():
-    heliq.helicity_function(np.random.standard_normal((2, 2, 2)), delta_alpha=3)
+    heliq.helicity_function(np.random.standard_normal((2, 2, 2)), 1., delta_alpha=3)
 
 
 def test_returns_helicityfunction_object():
-    rvalue = heliq.helicity_function(np.random.standard_normal((2, 2, 2)))
+    rvalue = heliq.helicity_function(np.random.standard_normal((2, 2, 2)), 1.)
     assert hasattr(rvalue, 'delta_alpha')
     assert hasattr(rvalue, 'delta_rho')
     assert hasattr(rvalue, 'histogram')
